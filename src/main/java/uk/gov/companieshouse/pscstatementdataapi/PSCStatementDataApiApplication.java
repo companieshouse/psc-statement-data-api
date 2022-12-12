eaf5d985d04abfda8b7ca444c2af31261b035b7d
lbedrosyanCH <lbedrosyan@companieshouse.gov.uk>
2022-12-12 15:47:23 +0000

    Added the pom, Tilt file and the main class

---
+++ src/main/java/uk/gov/companieshouse/pscstatementdataapi/PSCStatementDataApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.pscstatementdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PSCStatementDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PSCStatementDataApiApplication.class, args);
+    }
+
+}
