f134364d292012074fc57e32f3f7b9f207617ec0
maddytjCH <mtjalma@companieshouse.gov.uk>
2022-12-21 13:56:44 +0000

    adding mongo config

--- src/main/java/uk/gov/companieshouse/pscstatementdataapi/repository/PscStatementRepository.java
+++ src/main/java/uk/gov/companieshouse/pscstatementdataapi/repository/PscStatementRepository.java
@@ -2,13 +2,15 @@ package uk.gov.companieshouse.pscstatementdataapi.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+
 import uk.gov.companieshouse.pscstatementdataapi.model.PscStatementDao;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PscStatementRepository extends MongoRepository<PscStatementDao, String> {
 
     @Query("{'company_number' : ?0, '_id' : ?1}")
-    List<PscStatementDao> getPscStatementByCompanyNumberAndStatementId(String companyNumber, String statementId);
+    Optional<PscStatementDao> getPscStatementByCompanyNumberAndStatementId(String companyNumber, String statementId);
     }
 
--- src/main/java/uk/gov/companieshouse/pscstatementdataapi/services/RetrievePscStatementService.java
+++ src/main/java/uk/gov/companieshouse/pscstatementdataapi/services/RetrievePscStatementService.java
@@ -3,22 +3,36 @@ package uk.gov.companieshouse.pscstatementdataapi.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.companieshouse.pscstatementdataapi.model.PscStatementDao;
 import uk.gov.companieshouse.pscstatementdataapi.repository.PscStatementRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
 
 public class RetrievePscStatementService {
 
+  @Autowired
+  PscStatementRepository pscStatementRepository;
 
-    @Autowired
-    PscStatementRepository pscStatementRepository;
+  public PscStatementDao retrievePscStatementFromDb(
+    String companyNumber,
+    String statementId
+  ) throws JsonProcessingException {
+    Optional<PscStatementDao> statementOptional = pscStatementRepository.getPscStatementByCompanyNumberAndStatementId(
+      companyNumber,
+      statementId
+    );
+    PscStatementDao pscStatementDao = statementOptional.orElseThrow(() ->
+      new IllegalArgumentException(String.format(
+                "Resource not found for statement ID: %s, and company number: %f", statementId, companyNumber))
+    );
+    this.transformPscStatement(pscStatementDao);
+    return null;
+  }
 
-    public PscStatementDao retrievePscStatementFromDb(String companyNumber, String statementId){
-        pscStatementRepository.getPscStatementByCompanyNumberAndStatementId(companyNumber, statementId);
-        this.transformPscStatement();
-        return null;
-    }
-
-
-    public PscStatementDao transformPscStatement(){
-
-        return null;
-    }
+  public PscStatementDao transformPscStatement(PscStatementDao pscStatementDao)
+    throws JsonProcessingException {
+    String response = pscStatementDao.toString();
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.readValue(response, PscStatementDao.class);
+    return null;
+  }
 }
