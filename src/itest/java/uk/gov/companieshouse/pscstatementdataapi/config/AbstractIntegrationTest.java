6dbbdb1488c14c76a60001952703ffe101073c77
JAndrewCH <jandrew@companieshouse.gov.uk>
2023-03-16 15:54:31 +0000

    add integration tests

---
+++ src/itest/java/uk/gov/companieshouse/pscstatementdataapi/config/AbstractIntegrationTest.java
@@ -0,0 +1,20 @@
+package uk.gov.companieshouse.pscstatementdataapi.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.companieshouse.pscstatementdataapi.api.PscStatementApiService;
+
+/**
+ * Loads the application context.
+ * Best place to mock your downstream calls.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+@ActiveProfiles({"test"})
+public abstract class AbstractIntegrationTest extends AbstractMongoConfig {
+
+    @MockBean
+    public PscStatementApiService pscStatementApiService;
+}
