1a44f05b95fe1a52b805e54399bfca3430622312
mnycz-ch <167440954+mnycz-ch@users.noreply.github.com>
2024-05-15 09:10:22 +0100

    Update ExceptionHandlerConfig.java

--- src/main/java/uk/gov/companieshouse/pscstatementdataapi/config/ExceptionHandlerConfig.java
+++ src/main/java/uk/gov/companieshouse/pscstatementdataapi/config/ExceptionHandlerConfig.java
@@ -6,7 +6,6 @@ import com.mongodb.MongoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -15,8 +14,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.MethodNotAllowedException;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.api.exception.BadRequestException;
-import uk.gov.companieshouse.api.exception.ResourceNotFoundException;
 import uk.gov.companieshouse.api.exception.ServiceUnavailableException;
+import uk.gov.companieshouse.pscstatementdataapi.exception.ResourceNotFoundException;
+
 import java.time.format.DateTimeParseException;
 
 @ControllerAdvice
