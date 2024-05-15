64c9a6b8c33d1d4e0fc56b7d3aa370775db6d155
mnycz-ch <167440954+mnycz-ch@users.noreply.github.com>
2024-05-15 13:07:15 +0100

    remove unused imports

--- src/test/java/uk/gov/companieshouse/pscstatementdataapi/controller/PscStatementControllerTest.java
+++ src/test/java/uk/gov/companieshouse/pscstatementdataapi/controller/PscStatementControllerTest.java
@@ -2,8 +2,6 @@ package uk.gov.companieshouse.pscstatementdataapi.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
