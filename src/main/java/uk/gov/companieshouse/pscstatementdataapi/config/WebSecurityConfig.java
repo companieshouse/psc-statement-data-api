26dbcbb7e2570a94da7ad100b43508f82d391a5b
bcullerton <bcullerton@companieshouse.gov.uk>
2024-07-08 14:38:30 +0100

    Add cors filter to statement data api

--- src/main/java/uk/gov/companieshouse/pscstatementdataapi/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/companieshouse/pscstatementdataapi/config/WebSecurityConfig.java
@@ -7,15 +7,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfFilter;
 
+import uk.gov.companieshouse.api.filter.CustomCorsFilter;
 import uk.gov.companieshouse.api.interceptor.InternalUserInterceptor;
 import uk.gov.companieshouse.api.interceptor.UserAuthenticationInterceptor;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig implements WebMvcConfigurer {
 
-    //Methods that will not go through internal role validation
-    List<String> externalMethods = Arrays.asList("GET");
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(new CustomCorsFilter(externalMethods()), CsrfFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public List<String> externalMethods() {
+        return Arrays.asList("GET");
+    }
     //Key type is automatically checked by the authenticator add other allowed auth types here
     List<String> otherAllowedAuthMethods = Arrays.asList("oauth2");
 
@@ -31,6 +49,6 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public UserAuthenticationInterceptor userAuthenticationInterceptor() {
-        return new UserAuthenticationInterceptor(externalMethods, otherAllowedAuthMethods, internalUserInterceptor());
+        return new UserAuthenticationInterceptor(externalMethods(), otherAllowedAuthMethods, internalUserInterceptor());
     }
 }
